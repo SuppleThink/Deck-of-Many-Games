f05fbe4448617f9c4770511e90e7347cce883661
JE <je@bort.ninja>
2023-11-04 08:08:48 -0700

    set font preload to fix FOUC

--- app.vue
+++ app.vue
@@ -9,6 +9,52 @@
   <AppFooter />
 </template>
 
+<script setup type="ts">
+import IBM_VGA from "/fonts/Web437_IBM_VGA_9x16.woff"
+import NEC_MultiSpeed from "/fonts/Web437_NEC_MultiSpeed.woff"
+import ToshibaT300 from "/fonts/Web437_ToshibaT300_8x8.woff"
+import ToshibaSat from "/fonts/WebPlus_ToshibaSat_8x8.woff"
+
+if(process.server){
+  useHead({
+    link: [
+      {
+        rel: 'preload',
+        href: IBM_VGA,
+        crossorigin: '',
+        as: 'font',
+        type: 'font/woff2',
+
+      },
+      {
+        rel: 'preload',
+        href: NEC_MultiSpeed,
+        crossorigin: '',
+        as: 'font',
+        type: 'font/woff2',
+
+      },
+      {
+        rel: 'preload',
+        href: ToshibaT300,
+        crossorigin: '',
+        as: 'font',
+        type: 'font/woff2',
+
+      },
+      {
+        rel: 'preload',
+        href: ToshibaSat,
+        crossorigin: '',
+        as: 'font',
+        type: 'font/woff2',
+
+      },
+    ]
+  })
+}
+</script>
+
 <style scoped>
 .container {
   padding-top: 5em;
